3e32026b07cd6b8ae35a7b7f67da2ca78439231c
Scott Watermasysk <scottwater@gmail.com>
2019-11-12 07:14:02 -0500

    Webpack fonts

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,18 @@ module.exports = env => {
             { loader: "postcss-loader" },
             { loader: "sass-loader" }
           ]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "fonts/"
+              }
+            }
+          ]
         }
       ]
     },
