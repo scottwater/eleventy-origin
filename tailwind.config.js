7c6173075c0a033e0501e958b14738f0f18317fc
Scott Watermasysk <scottwater@gmail.com>
2020-08-24 10:48:29 -0400

    remove tailwind forms since tailwindUI is prefered

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const fonts = defaultTheme.fontFamily.sans;
 module.exports = {
-  plugins: [require("@tailwindcss/custom-forms")],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...fonts]
+        sans: ["Inter", ...fonts],
       },
       screens: {
-        dm: { raw: "(prefers-color-scheme: dark)" }
-      }
-    }
-  }
+        dm: { raw: "(prefers-color-scheme: dark)" },
+      },
+    },
+  },
 };
